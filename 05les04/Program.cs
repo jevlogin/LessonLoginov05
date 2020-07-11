073936e324a2a813db4155092e37b4c92ccf43f2
jevlogin <jev.login@mail.ru>
2020-07-12 02:31:57 +0300

    Очень важные методы обработки — разделения строки на элементы Split и слияние массива строк в единую строку Join

---
+++ 05les04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05les04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string poems = "белеет парус одинокий в тумане моря голубом";
+            char[] div = { ' ' };
+            string[] parts = poems.Split(div);
+            Console.WriteLine($"Результат разбиения на части: ");
+            for (int i = 0; i < parts.Length; i++)
+            {
+                Console.WriteLine(parts[i]);
+            }
+            string str = String.Join("|", parts);
+            Console.WriteLine("Результат сборки: ");
+            Console.WriteLine(str);
+
+            Console.ReadKey();
+        }
+    }
+}
