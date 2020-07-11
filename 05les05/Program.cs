d2779aeadfc090241f03e0a9b74329be5cd08fc1
jevlogin <jev.login@mail.ru>
2020-07-12 02:35:09 +0300

    Compare и CompareTo Особенно стоит отметить, как сравниваются строки. Так как в C# нельзя сравнивать строки с помощью операций отношений (<,>), для их сравнения нужно использовать методы Compare или CompareTo.

---
+++ 05les05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05les05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello!";
+            string s2 = "Hello!";
+            Console.WriteLine(String.Compare(s1, s2));
+            Console.WriteLine(s1.CompareTo(s2));
+
+            Console.ReadKey();
+        }
+    }
+}
