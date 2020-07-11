421e8cf254adf1220bc488fe299e527a841d2ec5
jevlogin <jev.login@mail.ru>
2020-07-12 02:40:24 +0300

    StringBuilder Чтобы создать строку, которую можно изменять, в С# предусмотрен класс StringBuilder, определённый в пространстве имён System.Text. Объекты этого класса всегда объявляются с явным вызовом конструктора класса (через операцию new) .

---
+++ 05les06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05les06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создание пустой строки, размер по умолчанию 16 символов
+            StringBuilder a = new StringBuilder();
+            // инициализация строки и выделение необходимой памяти
+            StringBuilder b = new StringBuilder("abcd");
+            // создание пустой строки и выделение памяти под 100 символов
+            StringBuilder c = new StringBuilder(100);
+            // инициализация строки и выделение памяти под 100 символов 
+            StringBuilder d = new StringBuilder("bcde", 100);
+            // инициализация подстроки "bcd" и выделение памяти под 100 символов	
+            StringBuilder e = new StringBuilder("abcd", 1, 3, 100);
+
+            Console.WriteLine(e);
+
+            Console.ReadKey();
+        }
+    }
+}
