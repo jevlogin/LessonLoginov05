f80954e2448f50330a459194c609d8321e607ecf
jevlogin <jev.login@mail.ru>
2020-07-12 14:37:28 +0300

    Regex regex = new Regex("Николай"); //  Создание регулярного выражения

---
+++ 05les08/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _05les08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data1 = "Петра, Андрей, Николай";
+            string data2 = "Петр, Андрей, Александр";
+            Regex regex = new Regex("Николай"); //  Создание регулярного выражения
+            Console.WriteLine(regex.IsMatch(data1));
+            Console.WriteLine(regex.IsMatch(data2));
+
+            Console.ReadKey();
+        }
+    }
+}
