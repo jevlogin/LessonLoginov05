7b202ce7c35d094c0e56d3e02bc469049c9d58f4
jevlogin <jev.login@mail.ru>
2020-07-12 13:20:31 +0300

    Работа со StringBuilder

--- 05les07/Program.cs
+++ 05les07/Program.cs
@@ -14,17 +14,45 @@ namespace _05les07
             {
                 StringBuilder str = new StringBuilder("Площадь");
                 PrintString(str);
+
+                str.Append(" треугольника равна");
+                PrintString(str);
+
+                str.AppendFormat(" {0:f2}", 123.456);
+                PrintString(str);
+
+                str.Insert(8, "данного ");
+                PrintString(str);
+
+                str.Remove(7, 21);
+                PrintString(str);
+
+                str.Replace("а", "о");
+                PrintString(str);
+
+                Console.WriteLine("Введите первую строку для сравнения");
+                StringBuilder str1 = new StringBuilder(Console.ReadLine());
+
+                Console.WriteLine("Введите вторую строку для сравнения");
+                StringBuilder str2 = new StringBuilder(Console.ReadLine());
+
+                Console.WriteLine($"Строки равны: {str1.Equals(str2)}");
             }
             catch (Exception)
             {
-
-                throw;
+                Console.WriteLine("Возникло исключение");
             }
+
+            Console.ReadKey();
         }
 
         private static void PrintString(StringBuilder str)
         {
-            Console.WriteLine(str);
+            Console.WriteLine($"Строка: {str}");
+            Console.WriteLine($"Текущая длина строки: {str.Length}");
+            Console.WriteLine($"Объем буфера: {str.Capacity}");
+            Console.WriteLine($"Максимальный объем буфера: {str.MaxCapacity}");
+            Console.WriteLine();
         }
     }
 }
