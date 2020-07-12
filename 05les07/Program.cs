c2628d57801519e01da62ec1338448a41f987062
jevlogin <jev.login@mail.ru>
2020-07-12 13:10:12 +0300

    Начинаем работать со StringBuilder

---
+++ 05les07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05les07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StringBuilder str = new StringBuilder("Площадь");
+                PrintString(str);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private static void PrintString(StringBuilder str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
