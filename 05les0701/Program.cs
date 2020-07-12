feeabd63a456adb3755169746dea3c705c5a762b
jevlogin <jev.login@mail.ru>
2020-07-12 13:24:27 +0300

    С изменяемой строкой можно работать не только как с объектом, но и как с массивом символов:

---
+++ 05les0701/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+
+/**
+ * С изменяемой строкой можно работать не только как с объектом, но и как с массивом символов:
+ * 
+ * */
+
+
+namespace _05les0701
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder a = new StringBuilder("2*3=3*2");
+            Console.WriteLine(a);
+            int k = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (char.IsDigit(a[i]))
+                {
+                    k += int.Parse(a[i].ToString());
+                }
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
