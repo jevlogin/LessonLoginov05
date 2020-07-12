164e41149aeff2df8be33657ca5355cd07435a5f
jevlogin <jev.login@mail.ru>
2020-07-12 15:01:12 +0300

    REGEX - {2,6} - ОЧЕНЬ ВАЖНО!!! никаких пробелов!!!!! между 2 и 6

---
+++ 05les09/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _05les09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"[A-Za-z]+[\.A-Za-z0-9_-]*[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]{2,6}");
+                                 //   [A-Za-z]+[\.A-Za-z0-9_-]*[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]{2,6}
+            Console.WriteLine(regex.IsMatch("jev.login@gmail.com"));
+            Console.WriteLine(regex.IsMatch("jev.login@mail"));
+            Console.WriteLine(regex.IsMatch("jev.login"));
+
+            Console.ReadKey();
+        }
+    }
+}
